357e6b9456b0387e10487cb901a0b81e3b319097
BLxcwg666 <huixcwg@gmail.com>
2025-05-10 21:24:46 +0800

    fix: is fixed? idk

--- src/main/java/cn/xcnya/bantracker/styles/Island.java
+++ src/main/java/cn/xcnya/bantracker/styles/Island.java
@@ -3,7 +3,6 @@ package cn.xcnya.bantracker.styles;
 import cn.xcnya.bantracker.data.PunishmentData;
 import today.opai.api.OpenAPI;
 
-import java.text.SimpleDateFormat;
 import today.opai.api.enums.EnumNotificationType;
 
 public class Island implements TrackerStyle {
@@ -17,16 +16,16 @@ public class Island implements TrackerStyle {
     @Override
     public void print(int wdDiff, int stDiff, int lastWD, int lastST, PunishmentData data) {
         if (wdDiff <= 0 && stDiff <= 0) return;
-
-        SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
+        if (!openAPI.getGameStateManager().isHypixel()) return;
+        // 妊娠公鸡你为什么害我翻了2小时的原始码
 
         if (wdDiff > 0) {
-            String msg = "§eWatchdog§r banned " + wdDiff + " Player(s).";
+            String msg = String.format("§eWatchdog§r banned %d Player(s).", wdDiff);
             openAPI.popNotification(EnumNotificationType.WARNING, "Ban Tracker", msg, 3000);
         }
 
         if (stDiff > 0) {
-            String msg = "§6Staff§r banned " + stDiff + " Player(s).";
+            String msg = String.format("§6Staff§r banned %d Player(s).", stDiff);
             openAPI.popNotification(EnumNotificationType.WARNING, "Ban Tracker", msg, 3000);
         }
     }
