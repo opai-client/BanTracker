306cc323eded6184bd42818703f4bf3b00ef8863
SakuraNiroku <liangyin17@outlook.com>
2025-05-13 00:27:09 +0800

    refactor(data): impl PunishmentData.java

--- src/main/java/cn/xcnya/bantracker/data/PunishmentData.java
+++ src/main/java/cn/xcnya/bantracker/data/PunishmentData.java
@@ -11,26 +11,21 @@ import java.util.List;
  * @date 2025/5/8 04:58
  **/
 public class PunishmentData {
-    public Staff staff;
-    public Watchdog watchdog;
-    @Deprecated public List<History> history; // 你说得对但是 0个用法 //hehe
+    public Punishment staff;
+    public Punishment watchdog;
+    public List<History> history;
 
-    //staff和watchdog的field都不同，如果一样用一个class就可以了 //怪我？
-    public static class Staff {
-        @SerializedName("last_half_hour") public int lastHalfHour;
-        @Deprecated @SerializedName("last_day") public int lastDay;
-        public int total;
-    }
-    public static class Watchdog {
-        @SerializedName("last_minute") public int lastMinute;
-        @Deprecated @SerializedName("last_day") public int lastDay;
-        public int total;
+    public static class Punishment {
+        @SerializedName("last_minute") public int lastMinute = 0;
+        @SerializedName("last_half_hour") public int lastHalfHour = 0;
+        @SerializedName("last_day") public int lastDay = 0;
+        public int total = 0;
     }
 
     public static class History {
-        @Deprecated public long time;
-        @Deprecated @SerializedName("formated") public String formattedTime; // formated?
-        @Deprecated public boolean watchdog; // boolean是坏习惯 //i like
-        @Deprecated @SerializedName("number") public int count;
+        public long time;
+        @SerializedName("formated") public String formattedTime;
+        public boolean watchdog;
+        @SerializedName("number") public int count;
     }
 }
